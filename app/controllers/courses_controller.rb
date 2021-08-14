277dd94885f2ead87a0b389833aa3e2cfaffb1c4
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-08-14 16:11:15 -0400

    fixed db issue

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,6 @@
 class CoursesController < ApplicationController
+
+    def index
+        @courses = Course.where("season_code = 202103").order(course_code: :asc)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -91,6 +91,7 @@ end
 def clean_data(courses)
 
     records_w_guts = courses.each do |course|
+        # puts course["course_code"]
         course.delete("times_by_day")
         course["all_course_codes"] = course["all_course_codes"].join(', ')
         course["areas"] = course["areas"].join(', ')
@@ -112,7 +113,7 @@ total_counter = 1
 total_count = Dir.children("./db/ct-data").count
 
 
-## ACTUAL ITERATION THRU ALL JSON FILES
+# ACTUAL ITERATION THRU ALL JSON FILES
 
 Dir.children("./db/ct-data").each do |filename|
     ## OPEN FILE
@@ -189,3 +190,76 @@ Dir.children("./db/ct-data").each do |filename|
     puts "Done!"
 end
 
+    # ## OPEN FILE
+    # path = File.join(File.dirname(__FILE__), "/ct-data/202103.json")
+    # courses_raw_data = JSON.parse(File.read(path))
+
+    # ## PREPARE DATA
+    # puts "starting file #{total_counter} / #{total_count}"
+
+    # clean_data(courses_raw_data)
+
+
+    # season_json_str = "202103.json"
+    # season_code_str = season_json_str[0...6].to_i
+    # courses_in_season = Course.all.select {|course| course.season_code == season_code_str}
+    
+    # gut_objs = courses_in_season.select {|course| course.gut_index != nil }
+    # prof_objs = courses_in_season.select {|course| (course.average_professor != nil)}
+    # workload_objs = courses_in_season.select {|course| (course.average_workload != nil)}
+    # same_prof_tot_rating_objs = courses_in_season.select {|course| (course.average_rating_same_professors != nil)}
+    # same_prof_workload_objs = courses_in_season.select {|course| (course.average_workload_same_professors != nil)}
+    
+    # ## GET STATS ON ALL DATA FOR SEASON
+    # puts "getting percentiles"
+    
+    # get_percentile(gut_objs, "gut_index", "gut_percentile")
+    # get_percentile(prof_objs, "average_professor", "professor_percentile")
+    # get_percentile(workload_objs, "average_workload", "workload_percentile")
+    # get_percentile(same_prof_tot_rating_objs, "average_rating_same_professors", "same_professor_total_rating_percentile")
+    # get_percentile(same_prof_workload_objs, "average_workload_same_professors", "same_professor_workload_percentile")
+    
+    # ## GET STATS ON ALL DATA FOR SEASON
+    # puts "getting stats"
+    
+    # get_stats(gut_objs, "gut_index")
+    # get_stats(prof_objs, "average_professor")
+    # get_stats(prof_objs, "average_workload")
+    # get_stats(prof_objs, "average_rating_same_professors")
+    # get_stats(prof_objs, "average_workload_same_professors")
+    
+    
+    # ## GET SUBJECT PERCENTILES AND SUBJECT STATS FOR SEASON
+    
+    # puts "getting subject percentiles"
+
+    # subjects = courses_in_season.map {|course| course.subject}.uniq
+    
+    # subjects.each do |subject|
+    
+    #     courses_in_subject = courses_in_season.select {|course| course.subject == subject }
+    
+    #     gut_courses_in_subject = courses_in_subject.select {|course| course.gut_index != nil}
+    #     get_percentile(gut_courses_in_subject, "gut_index", "gut_percentile_subject")
+    #     get_stats(gut_courses_in_subject, "gut_index", true)
+    
+    #     prof_courses_in_subject = courses_in_subject.select {|course| course.average_professor != nil}
+    #     get_percentile(prof_courses_in_subject, "average_professor", "professor_percentile_subject")
+    #     get_stats(prof_courses_in_subject, "average_professor", true)
+    
+    #     workload_courses_in_subject = courses_in_subject.select {|course| course.average_workload != nil}
+    #     get_percentile(workload_courses_in_subject, "average_workload", "workload_percentile_subject")
+    #     get_stats(workload_courses_in_subject, "average_workload", true)
+    
+    #     prof_same_courses_in_subject = courses_in_subject.select {|course| course.average_rating_same_professors != nil}
+    #     get_percentile(prof_same_courses_in_subject, "average_rating_same_professors", "same_professor_total_rating_percentile_subject")
+    #     get_stats(prof_same_courses_in_subject, "average_rating_same_professors", true)
+    
+    #     workload_same_courses_in_subject = courses_in_subject.select {|course| course.average_workload_same_professors != nil}
+    #     get_percentile(workload_same_courses_in_subject, "average_workload_same_professors", "same_professor_workload_percentile_subject")
+    #     get_stats(workload_same_courses_in_subject, "average_workload_same_professors", true)
+    
+    # end
+    # ## UPDATE TERMIAL PROMPT
+    # total_counter += 1
+    # puts "Done!"
